992d62db2eafaed98be64ed8cd892761503551ea
Jonathan "Duke" Leto <jonathan@leto.net>
2018-06-16 14:15:55 -0700

    Fix name of haraka python implementation and add a basic setup.py and some failing tests

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup (name = 'verus_hash',
+       version = '0.1',
+       description = 'VerusHash - Quantum-Resistant variable length hash function')
---
+++ test.py
@@ -0,0 +1,20 @@
+import verus_hash
+from binascii import unhexlify, hexlify
+
+import unittest
+header_hex = ("XXXX")
+best_hash = 'XXX'
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.block_header = unhexlify(header_hex)
+        self.best_hash = best_hash
+
+    def test_verus_hash(self):
+        self.pow_hash = hexlify(verus_hash.header_hash(self.block_header))
+        self.assertEqual(self.pow_hash, self.best_hash)
+
+if __name__ == '__main__':
+    unittest.main()
+
