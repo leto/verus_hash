f4694260f018419500c24faf18dc8b9ed2b1a94f
Jonathan "Duke" Leto <jonathan@leto.net>
2018-06-16 23:17:20 -0700

    Outline the general verus_hash algorithm in python

--- verus_hash.py
+++ verus_hash.py
@@ -3,4 +3,27 @@ import copy
 import haraka
 
 def header_hash(x):
-    return "deadbeef"
+    return verus_hash(x)
+
+def verus_hash(x):
+    len  = x.length()
+    hash = ""
+    pos  = 0
+    buf  = ""
+
+    # put last result
+
+    while True:
+        if pos >= len
+            break
+        if( len - pos >= 32 )
+            # TODO copy 32 bytes, "full chunk"
+        else
+            i = len - pos
+            # TODO copy partial chunk and fill rest of buffer with zeros
+        h = haraka512(buf2,buf)
+
+        # TODO: nextOffset stuff
+
+        pos+= 32
+    return hash
