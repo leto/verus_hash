9fbebe8a1c1a84e3f4d74595f6b21bf67eb3e540
Jonathan "Duke" Leto <jonathan@leto.net>
2018-06-18 12:36:54 -0700

    Grab low and high nibbles from hash input

--- verus_hash.py
+++ verus_hash.py
@@ -6,10 +6,11 @@ def header_hash(x):
     return verus_hash(x)
 
 def verus_hash(x):
-    len  = x.length()
-    hash = ""
-    pos  = 0
-    buf  = []
+    len       = x.length()
+    hash      = ""
+    pos       = 0
+    buf       = x[0:31]
+    high, low = x[:1], x[1:2]
 
     # put last result
 
